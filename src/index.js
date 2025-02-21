02bbb2819920463586f137accda7a6be6ff217a6
gunelaliyeva <gunelaliyevaa07@gmail.com>
2025-02-21 21:05:14 +0400

    updates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
-import App from './App';
-import './index.css'
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/* const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <App/>
     </BrowserRouter>
+);
+*/
+
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const basename = process.env.PUBLIC_URL;
+
+ReactDOM.render(
+  <BrowserRouter basename={basename}>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
